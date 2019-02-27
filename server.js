e74c4a8625906e2360f7c2ae1ba4258ba29e8e7d
Szymon Polanowski <szympol@hotmail.com>
2019-02-27 15:54:16 +0100

    Configurate API+ Google console

--- server.js
+++ server.js
@@ -1,14 +1,53 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./views/config');
 var app = express();
+var googleProfile = {};
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+  clientID: config.GOOGLE_CLIENT_ID,
+  clientSecret: config.GOOGLE_CLIENT_SECRET,
+  callbackURL: config.CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+      id: profile.id,
+      displayName: profile.displayName
+    };
+    cb(null, profile);
+  }
+));
+
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res) {
-  res.render('./login');
+  res.render('./login', {user: req.user});
 });
 
-app.get('/auth/google', function(req, res) {
-  res.render('./content');
+app.get('/logged', function(req, res) {
+  res.render('./content', {user: googleProfile});
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/logged',
+    failureRedirect: '/'
+}));
 
 app.listen(3000);
---
+++ views/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '999455587190-vr2nr1vrknnhkahru8vv37ok1kb2j81r.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'E2F9KOFsOIpwQlimLcHe3KWL',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+  };
